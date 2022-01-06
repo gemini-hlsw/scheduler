8a0f08f59e9950bf6562dde1086fc08c4326f4cc
Sergio <sergio.troncoso@noirlab.edu>
2022-01-06 16:46:33 -0300

    move start and end dates to params instead of attributes

--- horizons/__init__.py
+++ horizons/__init__.py
@@ -2,10 +2,12 @@ from dataclasses import dataclass
 from datetime import datetime, timedelta
 import dateutil.parser
 from common.minimodel import Target, TargetTag, Site
+from common.helpers import dms2rad, hms2rad, angular_distance
 import numpy as np
 import numpy.typing as npt
 import requests
-from typing import Tuple
+from typing import Tuple, Union
+import logging
 
 import os
 
@@ -19,46 +21,6 @@ class EphemerisCoordinates:
     dec: npt.NDArray[float]
     time: npt.NDArray[float]
 
-    @staticmethod
-    def dms2deg(dms):
-
-        if dms is None:
-            return None
-
-        d = float(dms[0])
-        m = float(dms[1])
-        s = float(dms[2])
-        sign = dms[3]
-        dd = d + m / 60. + s / 3600.
-
-        if sign == '-':
-            dd *= -1.
-
-        return dd
-
-    @staticmethod
-    def dms2rad(dms):
-        if dms is None:
-            return None
-        
-        dd = EphemerisCoordinates.dms2deg(dms)
-        rad = dd * np.pi / 180.
-        return rad
-
-    @staticmethod
-    def hms2rad(hms):
-        if hms is None:
-            return None
-
-        h = float(hms[0])
-        m = float(hms[1])
-        s = float(hms[2])
-        hours = h + m / 60. + s / 3600.
-        rad = hours * np.pi / 12.
-
-        return rad
-
-
 class HorizonsClient:
     """
     API to interact with the Horizons service
@@ -70,13 +32,15 @@ class HorizonsClient:
 
     def __init__(self,
                  site: Site,
-                 path: str = 'horizons/data',
+                 path: str = os.path.join('horizons', 'data'),
+                 airmass: int = 3,
                  start: datetime = None,
                  end: datetime = None):
         self.path = path
         self.url = 'https://ssd.jpl.nasa.gov/horizons_batch.cgi'
         self.start = start
         self.end = end
+        self.airmass = airmass
         self.site = site
      
     
@@ -84,28 +48,19 @@ class HorizonsClient:
     def generate_horizons_id(designation: str) -> str:
         des = designation.lower()
         return HorizonsClient.bodies[des] if des in HorizonsClient.bodies else designation
-    
-    @staticmethod
-    def _angular_distance(ra1: float, dec1: float, ra2: float, dec2: float) -> float:
-        """
-        Calculate the angular distance between two points on the sky.
-        based on https://github.com/gemini-hlsw/lucuma-core/blob/master/modules/core/shared/src/main/scala/lucuma/core/math/Coordinates.scala#L52
-        """
-        φ1 = dec1
-        φ2 = dec2
-        delta_φ = dec2 - dec1
-        delta_λ = ra2 - ra1
-        a = np.sin(delta_φ / 2)**2 + np.cos(φ1) * np.cos(φ2) * np.sin(delta_λ / 2)**2
-        return 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
 
     @staticmethod
-    def interpolate(ra1: float, dec1: float, ra2: float, dec2: float, f: float) -> float:
+    def interpolate(ra1: Union[npt.NDArray[float], float],
+                    dec1: Union[npt.NDArray[float], float],
+                    ra2: Union[npt.NDArray[float], float],
+                    dec2: Union[npt.NDArray[float], float],
+                    f: Union[npt.NDArray[float], float]) -> float:
         """
         Interpolate between two coordinates.
         based on https://github.com/gemini-hlsw/lucuma-core/blob/master/modules/core/shared/src/main/scala/lucuma/core/math/Coordinates.scala#L63-L89
         """
         # calculate angular distance to radians
-        delta = HorizonsClient.angular_distance(ra1, dec1, ra2, dec2)
+        delta = angular_distance(ra1, dec1, ra2, dec2)
         if delta == 0:
             return ra1, dec1
         else:
@@ -122,7 +77,38 @@ class HorizonsClient:
             φi = np.arctan2(z, np.sqrt(x * x + y * y))
             λi = np.arctan2(y, x)
             return λi, φi # there is a transformation here
+    
+    @staticmethod
+    def _bracket(times: np.array, time: datetime) -> Tuple[datetime, datetime]:
+        """
+        Return both lower and upper of the given time: i.e., the closest elements on either side. 
+        """
+        return times[times > time].min(), times[times < time].max()
 
+    @staticmethod
+    def interpolate_ephemeris(ephemeris: EphemerisCoordinates, time: datetime) -> Tuple[float, float]:
+        """
+        Interpolate ephemeris to a given time.
+        """
+        a, b = HorizonsClient._bracket(ephemeris.time, time)
+        # Find indexes for each bound
+        i_a, i_b = np.where(ephemeris.time == a)[0][0], np.where(ephemeris.time == b)[0][0]
+        factor = (time - a / b - a).timestamp() * 1000
+        logging.info()
+        ra, dec = HorizonsClient.interpolate(ephemeris.ra[i_a],
+                                             ephemeris.dec[i_a],
+                                             ephemeris.ra[i_b],
+                                             ephemeris.dec[i_b],
+                                             factor)
+        return ra, dec
+    
+    @staticmethod
+    def _time_bounds(start: datetime, end: datetime) -> Tuple[str, str]:
+        """
+        Returns the start and end times based on the given date
+        """
+        return start.strftime('%Y%m%d_%H%M'), end.strftime('%Y%m%d_%H%M')
+    
     def _form_horizons_name(self, tag: TargetTag, designation: str) -> str:
         """
         Formats the name of the body
@@ -135,29 +121,24 @@ class HorizonsClient:
             name = self.generate_horizons_id(designation)
         return name
 
-    def _time_bounds(self) -> Tuple[str, str]:
-        """
-        Returns the start and end times based on the given date
-        """
-        return self.start.strftime('%Y%m%d_%H%M'), self.end.strftime('%Y%m%d_%H%M')
-
-    def _get_ephemeris_file(self, name: str) -> str:
+    def _get_ephemeris_file(self, name: str, start: datetime, end: datetime) -> str:
         """
         Returns the ephemeris file name
         """
-        start, end = self._time_bounds()
+        start, end = HorizonsClient._time_bounds(start, end)
         return os.path.join(self.path, f"{self.site.name}_{name.replace(' ', '').replace('/', '')}_{start}-{end}.eph")
     
     def query(self,
               target: str,
+              start: datetime,
+              end: datetime,
               step: str = '1m',
               make_ephem: str = 'YES',
               cal_format: str = 'CAL',
               quantities: str = '1',
               object_data: str = 'NO',
               daytime: bool = False,
-              csvformat: str = 'NO',
-              airmass: int = 3) -> requests.Response:
+              csvformat: str = 'NO') -> requests.Response:
      
         # The items and order follow the JPL/Horizons batch example:
         # ftp://ssd.jpl.nasa.gov/pub/ssd/horizons_batch_example.long
@@ -178,8 +159,10 @@ class HorizonsClient:
         params['COORD_TYPE'] = None            # Type of user coordinates in SITE_COORD
         params['SITE_COORD'] = None            # '0,0,0'
         
-        params['START_TIME'] = self.start      # Ephemeris start time YYYY-MMM-DD {HH:MM} {UT/TT}
-        params['STOP_TIME']  = self.end        # Ephemeris stop time YYYY-MMM-DD {HH:MM}
+        # print(self.start.strftime("'%Y-%b-%d %H:%M'"))
+        # print(self.end.strftime("'%Y-%b-%d %H:%M'"))
+        params['START_TIME'] = start.strftime("'%Y-%b-%d %H:%M'")      # Ephemeris start time YYYY-MMM-DD {HH:MM} {UT/TT}
+        params['STOP_TIME']  = end.strftime("'%Y-%b-%d %H:%M'")        # Ephemeris stop time YYYY-MMM-DD {HH:MM}
         params['STEP_SIZE']  = "'" + step + "'" # Ephemeris step: integer# {units} {mode}
         params['TLIST']      = None            # Ephemeris time list
 
@@ -202,7 +185,7 @@ class HorizonsClient:
         params['ELEV_CUT']   = '-90'           # OBS: skip output when below elevation
         params['SKIP_DAYLT'] = skip_day   # OBS: skip output when daylight
         params['SOLAR_ELONG'] = "'0,180'"      # OBS: skip output outside range
-        params['AIRMASS']    = airmass    # OBS: skip output when airmass is > cutoff
+        params['AIRMASS']    = self.airmass    # OBS: skip output when airmass is > cutoff
         params['LHA_CUTOFF'] = None            # OBS: skip output when hour angle is > cutoff
         params['EXTRA_PREC'] = 'YES'           # OBS: show additional output digits (YES or NO)
         params['CSV_FORMAT'] = csvformat  # Output in comma-separated value format (YES or NO)
@@ -216,66 +199,55 @@ class HorizonsClient:
 
         return response
 
-    def get_ephemerides(self, target: Target, overwrite: bool = False) -> EphemerisCoordinates:
+    def get_ephemerides(self,
+                        target: Target,
+                        start: datetime,
+                        end: datetime,
+                        overwrite: bool = False) -> EphemerisCoordinates:
  
         horizons_name = self._form_horizons_name(target.tag, target.des)
-        file = self._get_ephemeris_file(horizons_name)
+        logging.info(f'{target.des}')
+        file = self._get_ephemeris_file(target.des, start, end)
 
         if not overwrite and os.path.exists(file):
+            logging.info(f'Saving ephemerides file for {target.des}')
             with open(file, 'r') as f:
                 lines = f.readlines()
         
         else:
-            res = self.query(horizons_name)
+            logging.info(f'Querying JPL/Horizons for {horizons_name}')
+            res = self.query(horizons_name, start, end)
             lines = res.text.splitlines()
             if file != None:
                 with open(file, 'w') as f:
                     f.write(res.text)
 
-        firstline = lines.index('$$SOE') + 1
-        lastline = lines.index('$$EOE') - 1
         time = np.array([])
         ra = np.array([])
         dec = np.array([])
 
-        for line in lines[firstline:lastline]:
-            if line and line[7:15] != 'Daylight' and line[7:14] != 'Airmass':
+        try:
+            firstline = lines.index('$$SOE') + 1
+            lastline = lines.index('$$EOE') - 1
 
-                values = line.split(' ')
-                values = d.split(' ')
-                rah = int(values[-6])
-                ram = int(values[-5])
-                ras = float(values[-4])
-                decg = values[-3][0] # sign
-                decd = int(values[-3][1:3])
-                decm = int(values[-2])
-                decs = float(values[-1])
+            for line in lines[firstline:lastline]:
+                if line and line[7:15] != 'Daylight' and line[7:14] != 'Airmass':
 
-                time = np.append(time, dateutil.parser.parse(line[1:18]))
-                ra = np.append(ra, EphemerisCoordinates.hms2rad([rah, ram, ras]))
-                dec = np.append(dec, EphemerisCoordinates.dms2rad([decd, decm, decs, decg]))
+                    values = line.split(' ')
+                    rah = int(values[-6])
+                    ram = int(values[-5])
+                    ras = float(values[-4])
+                    decg = values[-3][0] # sign
+                    decd = int(values[-3][1:3])
+                    decm = int(values[-2])
+                    decs = float(values[-1])
+
+                    time = np.append(time, dateutil.parser.parse(line[1:18]))
+                    ra = np.append(ra, hms2rad([rah, ram, ras]))
+                    dec = np.append(dec, dms2rad([decd, decm, decs, decg]))
+        except ValueError as e:
+            logging.error(f'Error parsing ephemerides file for {target.des}')
+            logging.error(e)
+            raise e
         
         return EphemerisCoordinates(ra, dec, time)
-
-    @staticmethod
-    def _bracket(times: np.array, time: datetime) -> Tuple[datetime, datetime]:
-        """
-        Return both lower and upper of the given time: i.e., the closest elements on either side. 
-        """
-        return times[times > time].min(), times[times < time].max()
-
-    @staticmethod
-    def interpolate_ephemeris(ephemeris: EphemerisCoordinates, time: datetime) -> Tuple[float, float]:
-        """
-        Interpolate ephemeris to a given time.
-        """
-        a, b = HorizonsClient._bracket(ephemeris.time, time)
-        # Find indexes for each bound
-        i_a, i_b = np.where(ephemeris.time == a)[0][0], np.where(ephemeris.time == b)[0][0]
-        factor = (time - a / b - a).timestamp() * 1000
-        ra, dec = HorizonsClient.interpolate(ephemeris.ra[i_a], 
-                                             ephemeris.dec[i_a], 
-                                             ephemeris.ra[i_b], 
-                                             ephemeris.dec[i_b], 
-                                             factor)
-        return ra, dec
--- test_horizons.py
+++ test_horizons.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
 
     site = Site.GS
     today = datetime.today()
-    client = HorizonsClient(Site.GS, start= today-timedelta(days=1), end= today)
+    client = HorizonsClient(Site.GS, airmass=300)
     target = NonsiderealTarget('Beer', None, type=TargetType.BASE, tag=TargetTag.COMET, des='1971 UC1', ra=None, dec=None)
-    eph = client.get_ephemerides(target)
+    eph = client.get_ephemerides(target,start= today-timedelta(days=1), end= today)
     target.ra = eph.ra
     target.dec = eph.dec
     print(target)
