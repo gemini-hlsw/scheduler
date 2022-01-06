fa2bd7f90f44c6672928d85972800d7bfb183630
Sergio <sergio.troncoso@noirlab.edu>
2022-01-06 17:56:57 -0300

    add context manager for horizons

--- test_horizons.py
+++ test_horizons.py
@@ -1,4 +1,4 @@
-from horizons import HorizonsClient
+from horizons import horizons_session
 from common.minimodel import Site, NonsiderealTarget, TargetTag, TargetType
 from datetime import datetime, timedelta
 
@@ -6,9 +6,12 @@ if __name__ == '__main__':
 
     site = Site.GS
     today = datetime.today()
-    client = HorizonsClient(Site.GS, airmass=300)
-    target = NonsiderealTarget('Beer', None, type=TargetType.BASE, tag=TargetTag.COMET, des='1971 UC1', ra=None, dec=None)
-    eph = client.get_ephemerides(target,start= today-timedelta(days=1), end= today)
-    target.ra = eph.ra
-    target.dec = eph.dec
-    print(target)
+    target = NonsiderealTarget('Beer', None, type=TargetType.BASE, 
+                                tag=TargetTag.COMET, des='1971 UC1', ra=None, dec=None)
+
+
+    with horizons_session(site, today-timedelta(days=1), today, 300) as session:
+        eph = session.get_ephemerides(target, start=today-timedelta(days=1), end=today)
+        target.ra = eph.ra
+        target.dec = eph.dec
+        print(target)
