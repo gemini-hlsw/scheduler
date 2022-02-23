1ed731bac86d8e49dbc272acdb1e607c3093a9c6
Sebastian Raaphorst <sraaphorst@gmail.com>
2022-02-22 21:44:11 -1000

    SCHED-97: Collector script cleanup.

--- scripts/run_collector.py
+++ scripts/run_collector.py
@@ -4,6 +4,7 @@ from api.ocs import read_ocs_zipfile, OcsProgramProvider
 from common.output import print_collector_info
 from collector import *
 
+
 if __name__ == '__main__':
     # Reduce logging to ERROR only to display the tqdm bars nicely.
     # logging.basicConfig(level=logging.INFO)
@@ -12,23 +13,18 @@ if __name__ == '__main__':
     # Read in a list of JSON data
     programs = read_ocs_zipfile(os.path.join('..', 'data', '2018B_program_samples.zip'))
 
-    start_time = Time("2018-10-01 08:00:00", format='iso', scale='utc')
-    end_time = Time("2018-10-03 08:00:00", format='iso', scale='utc')
-    time_slot_length = 1.0 * u.min
-    sites = {Site.GN, Site.GS}
-    semesters = {Semester(2018, SemesterHalf.B)}
-
+    # Create the Collector and load the programs.
     collector = Collector(
-        start_time=start_time,
-        end_time=end_time,
-        time_slot_length=time_slot_length,
-        sites=sites,
-        semesters=semesters,
+        start_time=Time("2018-10-01 08:00:00", format='iso', scale='utc'),
+        end_time=Time("2018-10-03 08:00:00", format='iso', scale='utc'),
+        time_slot_length=1.0 * u.min,
+        sites={Site.GN, Site.GS},
+        semesters={Semester(2018, SemesterHalf.B)},
         program_types={ProgramTypes.Q, ProgramTypes.LP, ProgramTypes.FT, ProgramTypes.DD},
         obs_classes={ObservationClass.SCIENCE, ObservationClass.PROGCAL, ObservationClass.PARTNERCAL}
     )
-
     collector.load_programs(program_provider=OcsProgramProvider(),
                             data=programs)
 
+    # Output the state of and information calculated by the Collector.
     print_collector_info(collector, samples=60)
