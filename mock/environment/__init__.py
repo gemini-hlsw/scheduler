31e672d5182867bfb41eb4c764bc9aff26abd067
David W.K <david.welch@noirlab.edu>
2022-06-21 11:25:29 -1000

    SCHED-159:Copied the environment retrieval method from the Collector class to the Env class.

--- mock/environment/__init__.py
+++ mock/environment/__init__.py
@@ -1,2 +1,26 @@
+from common.minimodel import Site, Variant, CloudCover, ImageQuality, WaterVapor
+from astropy.time import Time
+from astropy.coordinates import Angle
+from astropy.units import Quantity
+from typing import Optional
+import numpy as np
+import astropy.units as u
+
 class Env:
-    ...
+    def get_actual_conditions_variant(self,
+                                      site: Site,
+                                      times: Time) -> Optional[Variant]:
+        """
+        Return the weather variant.
+        This should be site-based and time-based.
+        """
+        night_length = len(times)
+
+        return Variant(
+            iq=np.full(night_length, ImageQuality.IQ70),
+            cc=np.full(night_length, CloudCover.CC50),
+            wv=np.full(night_length, WaterVapor.WVANY),
+            wind_dir=Angle(np.full(night_length, 330.0), unit='deg'),
+            wind_sep=Angle(np.full(night_length, 40.0), unit='deg'),
+            wind_spd=Quantity(np.full(night_length, 5.0 * u.m / u.s))
+        )
