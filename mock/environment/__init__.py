18755d830c1f7e8f40843f1b5467ca1ec2461bba
David W.K <david.welch@noirlab.edu>
2022-07-06 10:13:57 -1000

    WIP

--- mock/environment/__init__.py
+++ mock/environment/__init__.py
@@ -18,32 +18,31 @@ class Env:
     def __init__(self):
         self.site_data = {}
         self.site_data_by_night = {}
-        
+        self.night_data = pd.DataFrame()
+
         for site in Site:
             site_lc = site.name.lower()
             input_filename = Env.data_file_path(f'{site_lc}_weather_data.pickle.bz2')
-            # count = 0
+            count = 0
 
             if(os.path.exists(input_filename)):
-                # logging.info(f'Reading {input_filename}')
+                logging.info(f'Reading {input_filename}')
                 with bz2.open(input_filename) as input_file:
                         input_data = pd.read_pickle(input_file)
                         self.site_data[site_lc] = input_data 
-                        # logging.info(site_lc)
-                        # logging.info(self.site_data[site_lc].iloc[0:2])
-                        # logging.info(f'\t{len(self.site_data[site_lc].columns)} columns, {len(self.site_data[site_lc])} rows')
-                # self.site_data_by_night[site_lc] = {}
-                # for night in self.site_data[site_lc]["Time_Stamp_UTC"]:
-                #     night_date = night.date()
-                #     night_date = night_date.strftime('%Y-%m-%d') 
-                #    # logging.info(night_date)
-                #     self.site_data_by_night[site_lc][night_date] = self.site_data[site_lc].iloc[count]
-                #     count += 1
-                #     print(self.site_data_by_night[site_lc][night_date])
+                        logging.info(site_lc)
+                        logging.info(self.site_data[site_lc].iloc[0:2])
+                        logging.info(f'\t{len(self.site_data[site_lc].columns)} columns, {len(self.site_data[site_lc])} rows')
+                self.site_data_by_night[site_lc] = {}
+                for night in self.site_data[site_lc]["Time_Stamp_UTC"]:
+                    night_date = night.date()
+                    night_date = night_date.strftime('%Y-%m-%d') 
+                   # logging.info(night_date)
+                    self.site_data_by_night[site_lc][night_date] = self.night_data.concat(self.site_data[site_lc].iloc[count])
             else:
                 print(f'Error, {input_filename} not found')
         
-       
+        print(self.site_data_by_night['gn']["2017-09-30"])
     def get_actual_conditions_variant(self,
                                       site: Site,
                                       times: Time) -> Optional[Variant]:
@@ -66,6 +65,6 @@ class Env:
     def data_file_path(filename: str) -> str:
         return os.path.join('..', '..', 'data', filename)
 
-# if __name__ == '__main__':
-#     logging.basicConfig(level=logging.INFO)
-#     env = Env()
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    env = Env()
