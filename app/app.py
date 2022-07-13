ecc556488013721d8ebf88a71ef610a542a81eb0
Sergio <sergio.troncoso@noirlab.edu>
2022-07-13 16:35:57 -0400

    SCHED-167: Modify to handle more then one subscription

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from .process_manager import ProcessManager, TaskType
 from .scheduler import Scheduler
-from common.queries import Session, test_subscription_query, connection
+from common.queries import Session, observation_update, program_update, target_update
 from astropy.time import Time
 
 
@@ -13,7 +13,7 @@ class App:
         self.config = config
         self.manager = ProcessManager(size=config.process_manager.size,
                                       timeout=config.process_manager.timeout)
-        self.session = Session()
+        self.session = Session(url=config.graphql.url)
 
     def build_scheduler(self, start_time: Time, end_time: Time):
         # TODO: This needs to be modified to use more robust way to build schedulers
@@ -26,18 +26,17 @@ class App:
         def shutdown():
             done.set()
             self.manager.shutdown()
+            [t.cancel() for t in asyncio.all_tasks()] 
             asyncio.get_event_loop().stop()
         
         asyncio.get_event_loop().add_signal_handler(signal.SIGINT, shutdown)
 
         while not done.is_set():
             
-            # Check for updates in the ODB
-            resp = await self.session.subscribe_all(self.config.graphql.url)
-
+            resp = await self.session.subscribe_all()
             if resp:
-                print(f"Change on ODB: {resp['observationEdit']['editType']} for {resp['observationEdit']['value']['id']} ")
-
+                print(resp)
+                # Run new Schedule
                 mode = TaskType.STANDARD
                 start = Time("2018-10-01 08:00:00", format='iso', scale='utc')
                 end = Time("2018-10-03 08:00:00", format='iso', scale='utc')
--- common/queries/session.py
+++ common/queries/session.py
@@ -1,59 +1,35 @@
 import asyncio
 import backoff
 from gql.transport.websockets import WebsocketsTransport
-from gql.transport.aiohttp import AIOHTTPTransport
 from gql import Client, gql
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 from common.queries import observation_update, program_update, target_update
 
 
-class Subscription:
-    def __init__(self, session, query):
-        self.session = session
-        self.query = query
-    
-    async def __aiter__(self):
-        for item in self.session.subscribe(self.query):
-            yield item
-
-
 class Session:
     
-    def __init__(self):
-        pass
+    def __init__(self, url: str = 'http://localhost:8080'):
+        self.url = url
     
-    async def query(self, query: gql):
+    async def _query(self, query: gql):
         return self.client.execute(query)
 
-    async def subscribe(self, session: Client, sub: gql):
+    async def _subscribe(self, session: Client, sub: gql):
         """
         Async generator for gql.Client.subscribe
         """
         async for res in session.subscribe(sub):
             return res
     
+    async def subscribe_all(self):
+        subs = [observation_update, program_update, target_update]
+        tasks = [asyncio.create_task(self.on_update(sub)) for sub in subs]
+        return await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+
     @backoff.on_exception(backoff.expo, Exception, max_time=300)
-    async def subscribe_all(self, url: str):
-        
-        client = Client(transport=WebsocketsTransport(url=f'wss://{url}/ws'))
-
+    async def on_update(self, query: gql):
+        client = Client(transport=WebsocketsTransport(url=f'wss://{self.url}/ws'))
         async with client as session:
-            return await self.subscribe(session, observation_update)
-        
-        #program_sub = asyncio.create_task(self._execute_sub(program_update))
-        #observation_sub = asyncio.create_task(self._execute_sub(observation_update))
-        # target_sub = asyncio.create_task(self._execute_sub(target_update))
-        # await asyncio.gather(program_sub, observation_sub, target_sub)
-
-
-async def execute(session: Client, subscription: gql):
-    async for res in session.subscribe(subscription):
-        print(res)
-    
-
-@backoff.on_exception(backoff.expo, Exception, max_time=300)
-async def connection(url: str):
-    client = Client(transport=WebsocketsTransport(url=f'wss://{url}/ws'))
-    
-    async with client as session:
-        await execute(session, observation_update)
+            return await self._subscribe(session, query)
