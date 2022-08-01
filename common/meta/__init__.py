759bb915a192d852e4352f7188b97da8be8c0531
Sergio <sergio.troncoso@noirlab.edu>
2022-08-01 15:05:22 -0400

    SCHED-196: Add singleton metaclass

---
+++ common/meta/__init__.py
@@ -0,0 +1,15 @@
+from threading import Lock
+
+
+class Singleton(type):
+    """
+    Thread-safe Singleton metaclass.
+    """
+    _instances = {}
+    _lock: Lock = Lock()
+
+    def __call__(cls, *args, **kwargs):
+        with cls._lock:
+            if cls not in cls._instances:
+                cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
