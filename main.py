738164909267186bf7d3485ee635d67aae38b474
Sergio <sergio.troncoso@noirlab.edu>
2022-08-18 13:04:05 -0400

    SCHED-202: Remove unnecessary dependency

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 import uvicorn
 from fastapi.responses import JSONResponse
 from app.graphql.schema import PlanManager
-from app.process_manager import ProcessManager, TaskType
-from app.scheduler import Scheduler
+from app.process_manager import ProcessManager
 from app.config import config
 from app import app
-from astropy.time import Time
-from datetime import datetime
-import asyncio
 
 plan_manager = PlanManager()
-
-
-async def new_schedule(manager, scheduler):
-    manager.add_task(datetime.now(), scheduler, TaskType.STANDARD)
-    await asyncio.sleep(10)
-
 
 # Root API
 @app.get("/", include_in_schema=False)
