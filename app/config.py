4ed5b17df39b3c4b4e391764130dcf39de1e362d
Sebastian Raaphorst <sraaphorst@gmail.com>
2022-12-04 23:43:32 -1000

    SCHED-268: WIP

--- app/config.py
+++ app/config.py
@@ -2,15 +2,7 @@
 # For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
 import os
-from dataclasses import dataclass
-from typing import FrozenSet, Union, List
 
-import astropy.units as u
-from astropy.time import TimeDelta
-from lucupy.minimodel.observation import ObservationClass
-from lucupy.minimodel.program import ProgramTypes
-from lucupy.minimodel.semester import Semester, SemesterHalf
-from lucupy.minimodel.site import Site, ALL_SITES
 from omegaconf import OmegaConf
 
 from definitions import ROOT_DIR
@@ -26,6 +18,7 @@ class ConfigurationError(Exception):
 
     def __init__(self, config_type: str, value: str):
         super().__init__(f'Configuration error: {config_type} {value} is invalid.')
+
 
 path = os.path.join(ROOT_DIR, 'config.yaml')
 config = OmegaConf.load(path)
--- app/core/builder/blueprint.py
+++ app/core/builder/blueprint.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
 # For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
-from typing import FrozenSet, Union, List, Any
+from typing import Any, FrozenSet, List, Type, Union, TypeVar
 from enum import Enum
 
 from astropy.time import TimeDelta
@@ -16,25 +16,29 @@ from app.core.components.optimizer.dummy import DummyOptimizer
 from mock.resource import ResourceMock
 from mock.environment import Env
 
+T = TypeVar('T')
 
-def parse_configuration(enum: Enum, value: str) -> Any:
+
+def parse_configuration(enum_class: Type[T], value: str) -> Any:
     """General parser for config.yml
 
 
     Args:
-        enum (Enum): Enum corresponding to the setting to parse
-        value (str): Value on config.yml
+        enum_class (Type): Enum corresponding to the setting to parse
+        value     (str): Value on config.yml
 
     Raises:
         ConfigurationError: General configuration error in case the string
 
     Returns:
-        Any: The represent selection on the Enum.
+        Any: The selection from the Enum.
     """
     try:
-        return enum[value]
+        return enum_class[value]
+    except TypeError:
+        raise ConfigurationError('Enum type', value)
     except ValueError:
-        raise ConfigurationError(enum.name, value)
+        raise ConfigurationError(enum_class.__name__, value)
 
 
 class Blueprint:
@@ -47,15 +51,20 @@ class CollectorBlueprint(Blueprint):
     """Blueprint for the Collector.
     This is based on the configuration in config.yml.
     """
+
     def __init__(self,
                  semesters: List[str],
                  obs_class: List[str],
                  prg_type: List[str],
-                 sites: Union[str,List[str]],
+                 sites: Union[str, List[str]],
                  time_slot_length: float) -> None:
-        self.semesters: FrozenSet[Semester] =  frozenset(map(CollectorBlueprint._parse_semesters, semesters))
-        self.obs_classes: FrozenSet[ObservationClass] = frozenset(map(lambda x: parse_configuration(ObservationClass, x), obs_class))
-        self.program_types: FrozenSet[ProgramTypes] = frozenset(map(lambda x: parse_configuration(ProgramTypes, x), prg_type))
+        self.semesters: FrozenSet[Semester] = frozenset(map(CollectorBlueprint._parse_semesters, semesters))
+        self.obs_classes: FrozenSet[ObservationClass] = frozenset(
+            map(lambda x: parse_configuration(ObservationClass, x), obs_class)
+        )
+        self.program_types: FrozenSet[ProgramTypes] = frozenset(
+            map(lambda x: parse_configuration(ProgramTypes, x), prg_type)
+        )
         self.sites: FrozenSet[Site] = CollectorBlueprint._parse_sites(sites)
         self.time_slot_length: TimeDelta = TimeDelta(time_slot_length * u.min)
 
@@ -83,7 +92,7 @@ class CollectorBlueprint(Blueprint):
         try:
             return Semester(int(year), e_half)
         except ValueError:
-            raise ConfigurationError('Semester year', year)    
+            raise ConfigurationError('Semester year', year)
 
     @staticmethod
     def _parse_sites(sites: Union[str, List[str]]) -> FrozenSet[Site]:
@@ -104,9 +113,10 @@ class CollectorBlueprint(Blueprint):
                 return Site.GN
             else:
                 raise ConfigurationError('Missing site', site)
+
         if sites == 'ALL_SITES':
-        # In case of ALL_SITES option, return lucupy alias for the set of all Site enums
-            return ALL_SITES 
+            # In case of ALL_SITES option, return lucupy alias for the set of all Site enums
+            return ALL_SITES
 
         if isinstance(sites, list):
             return frozenset(map(parse_specific_site, sites))
@@ -117,47 +127,49 @@ class CollectorBlueprint(Blueprint):
     def __iter__(self):
         return iter((self.time_slot_length,
                      self.sites,
-                     self.semesters, 
+                     self.semesters,
                      self.program_types,
                      self.obs_classes))
+
 
 class OptimizerBlueprint(Blueprint):
     """Blueprint for the Selector.
     This is based on the configuration in config.yml.
     """
-    
+
     def __init__(self, algorithm: str) -> None:
         self.algorithm = OptimizerBlueprint._parse_optimizer(algorithm)
-    
+
     @staticmethod
     def _parse_optimizer(algorithm_name: str):
         # TODO: Enums are needed but for now is just Dummy
         # TODO: When GMax is ready we can expand
-        if algorithm_name in 'DUMMY':
+        if algorithm_name.upper() == 'DUMMY':
             return DummyOptimizer()
         else:
             raise ConfigurationError('Optimizer', config.optimizer.name)
+
     def __iter__(self):
-        return iter((self.algorithm))
+        # return iter((self.algorithm))
+        return iter([self.algorithm])
+
 
 class SourcesBlueprint(Blueprint):
-    
     class ResourceSources(Enum):
         MOCK = ResourceMock()
         # TODO: As in full fledge service? I'm not sure about this name 
         # so suggestions are welcome. 
         FULL = None
-    
+
     class EnvSources(Enum):
         MOCK = Env()
         # TODO: This need to be hookup to the real service.
-        FULL = None 
-
+        FULL = None
 
     def __init__(self, resource_source: str, env_source: str):
         self.resource = parse_configuration(SourcesBlueprint.ResourceSources, resource_source).value
         self.environment = parse_configuration(SourcesBlueprint.EnvSources, env_source).value
-    
+
 
 class Blueprints:
     collector: CollectorBlueprint = CollectorBlueprint(config.collector.semesters,
