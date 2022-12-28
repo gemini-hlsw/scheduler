2d697c6e8279b7d8260e6cd08d1445e48355f777
Sergio <sergio.troncoso@noirlab.edu>
2022-12-28 20:36:27 -0300

    SCHED-220: Add pre_adder to check_headers script

--- scheduler/scripts/check_headers.py
+++ scheduler/scripts/check_headers.py
@@ -7,6 +7,16 @@ from definitions import ROOT_DIR
 
 # Checks the headers of all .py files to make sure that they contain the above Copyright message.
 
+
+HEADER = '# Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)\n# For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause'
+
+def pre_adder(file):
+    with open(file, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(HEADER + '\n' + content)
+
+
 if __name__ == '__main__':
     COPYRIGHT = '# Copyright'
 
@@ -20,6 +30,7 @@ if __name__ == '__main__':
         print('*** FILES MISSING COPYRIGHT ***')
         for bad_file in bad_files:
             print(bad_file)
+            pre_adder(bad_file)
         exit(1)
     else:
         print('All files copyrighted.')
