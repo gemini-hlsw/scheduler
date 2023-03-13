a80b00d53a87ee384760aecb42df8ea80ee4f28c
Sergio <sergio.troncoso@noirlab.edu>
2023-03-13 12:22:18 -0300

    SCHED-325: Remove old query

--- tests/unit/scheduler/graphql_mid/test_graphql_queries.py
+++ tests/unit/scheduler/graphql_mid/test_graphql_queries.py
@@ -2,37 +2,6 @@
 # For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 from scheduler.graphql_mid.server import  schema
 
-
-def test_planpersite_query():
-
-
-    query= """
-        query plansPerSite{
-            sitePlans(site: GS) {
-                nightIdx
-                plansPerSite {
-                    site
-                    startTime
-                    endTime
-                    visits {
-                        startTime
-                        obsId
-                        atomStartIdx
-                        atomEndIdx
-                    }
-                }
-            }
-        }
-    """
-
-    result = schema.execute_sync(query)
-    assert result.errors is None
-    # TODO: if we test for an exact solution right now as the Optimizer
-    # is WIP these always would break. We need to revisit this test
-    # when Gmax work is finished.
-    # assert len(result.data["sitePlans"]) > 0
-
-
 def test_schedule_query():
 
     query= """
