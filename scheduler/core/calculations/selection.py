03d0c10156b9102c8ee742e04694e50ccf74dcc9
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-04-04 12:38:50 -1000

    SCHED-340: WIP

--- scheduler/core/calculations/selection.py
+++ scheduler/core/calculations/selection.py
@@ -43,12 +43,6 @@ class Selection:
         else:
             return [Selection._get_obs_group_ids(subgroup) for subgroup in group.children]
 
-    @property
-    def obs_group_ids(self) -> FrozenSet[UniqueGroupID]:
-        return frozenset(flatten(
-            Selection._get_obs_group_ids(program_info.program.root_group) for program_info in self.program_info.values()
-        ))
-
     def __post_init__(self):
         object.__setattr__(self, 'program_ids', frozenset(self.program_info.keys()))
 
