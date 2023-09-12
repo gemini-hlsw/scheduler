e89be3e3185c7c192d5695b1a66a15452fdedcfb
Sergio <sergio.troncoso@noirlab.edu>
2023-09-12 12:24:35 -0300

    SCHED-455: Solves negative time loss

--- scheduler/core/plans/__init__.py
+++ scheduler/core/plans/__init__.py
@@ -214,6 +214,9 @@ class Plan:
     def time_left(self) -> int:
         return self._time_slots_left
 
+    def update_time_slots(self, time: int):
+        self._time_slots_left = time
+
     def __contains__(self, obs: Observation) -> bool:
         return any(visit.obs_id == obs.id for visit in self.visits)
 
