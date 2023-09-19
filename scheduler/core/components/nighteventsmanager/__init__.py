35e312971f7a90ebadf38cadc94f36481c3b4d2b
Sebastian Raaphorst <sraaphorst@gemini.edu>
2023-09-19 12:20:08 -1000

    SCHED-463: Fixed NightEventManager caching. (#316)
    * SCHED-463: Fixed NightEventManager caching.
    
    * SCHED-463: Removing cruft.
    
    * SCHED-463: Simplifying time ID.

--- scheduler/core/components/nighteventsmanager/__init__.py
+++ scheduler/core/components/nighteventsmanager/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
 # For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
-from typing import Dict
+from typing import Dict, Tuple
 
 from astropy.time import Time, TimeDelta
 from lucupy import sky
@@ -15,7 +15,8 @@ class NightEventsManager(metaclass=Singleton):
     """
     A singleton class that manages pre-calculations of NightEvents for each Site during the dates specified.
     """
-    _night_events: Dict[Site, NightEvents] = {}
+    _ID = Tuple[Site, TimeDelta, Time, Time]
+    _night_events: Dict[_ID, NightEvents] = {}
 
     @staticmethod
     def get_night_events(time_grid: Time,
@@ -25,22 +26,17 @@ class NightEventsManager(metaclass=Singleton):
         Retrieve NightEvents. These may contain more information than requested,
         but never less.
         """
-        ne = NightEventsManager._night_events
+        # The identifier used for caching.
+        data_id: NightEventsManager._ID = (site, time_slot_length, time_grid[0], time_grid[-1])
 
-        # Recalculate if we are not compatible.
-        if (site not in ne or
-                time_slot_length != ne[site].time_slot_length or
-                (len(ne[site].time_grid) == 1 and ne[site].time_grid[0] != time_grid[0]) or
-                (len(ne[site].time_grid) > 1 and
-                 (time_grid[0] < ne[site].time_grid[0] or time_grid[-1] > ne[site].time_grid[-1]))):
-            # For some strange reason, this does not work if we specify keywords for NightEvents.
-            # It complains about __init__() getting multiple args for time_grid.
+        # Recalculate if necessary.
+        if data_id not in NightEventsManager._night_events:
             night_events = NightEvents(
                 time_grid,
                 time_slot_length,
                 site,
                 *sky.night_events(time_grid, site.location, site.timezone)
             )
-            NightEventsManager._night_events[site] = night_events
+            NightEventsManager._night_events[data_id] = night_events
 
-        return NightEventsManager._night_events[site]
+        return NightEventsManager._night_events[data_id]
