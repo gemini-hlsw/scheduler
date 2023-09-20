d0695e35f7d91ed223e9f89a96722faeefde50e6
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-09-20 11:46:57 -1000

    WIP.

--- scheduler/services/chronicle/data/cleanup.py
+++ scheduler/services/chronicle/data/cleanup.py
@@ -1,24 +1,33 @@
+# Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
+# For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
 from datetime import datetime
 
-new_lines = []
 
-with open('EngTasksGS.txt', 'r') as f:
-    old_lines = f.readlines()
+def main():
+    new_lines = []
 
-for line in old_lines:
-    lines = line.split('\t')
-    print(lines)
-    if lines[1].strip() == '0' or lines[1].strip() == '0.0':
-        continue
-    date = lines[0]
-    first_date_str = date.split('-')[0]
-    date_obj = datetime.strptime(first_date_str, "%Y %b%d")
-    formatted_date_str = date_obj.strftime("%Y-%m-%-d")
+    with open('EngTasksGS.txt', 'r') as f:
+        old_lines = f.readlines()
 
-    start, end = lines[2].split('-')
-    msg = lines[4].strip() if len(lines) == 5 else lines[3].strip()
-    new_lines.append(f'{formatted_date_str}\t{start}\t{end}\t[{msg}]\n')
+    for line in old_lines:
+        lines = line.split('\t')
+        print(lines)
+        if lines[1].strip() == '0' or lines[1].strip() == '0.0':
+            continue
+        date = lines[0]
+        first_date_str = date.split('-')[0]
+        date_obj = datetime.strptime(first_date_str, "%Y %b%d")
+        formatted_date_str = date_obj.strftime("%Y-%m-%-d")
 
-# Write the file out again
-with open('EngTasksGS.txt', 'w') as file:
-    file.writelines(new_lines)
+        start, end = lines[2].split('-')
+        msg = lines[4].strip() if len(lines) == 5 else lines[3].strip()
+        new_lines.append(f'{formatted_date_str}\t{start}\t{end}\t[{msg}]\n')
+
+    # Write the file out again
+    with open('EngTasksGS.txt', 'w') as file:
+        file.writelines(new_lines)
+
+
+if __name__ == '__main__':
+    main()
