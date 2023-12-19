0e500570409519f43062c66bcc4e8620cd24f4d8
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-12-19 12:06:50 -1000

    SCHED-532: RankerParameters should not be ClassVars and should be initialized with default_factory.

--- scheduler/core/components/ranker/default.py
+++ scheduler/core/components/ranker/default.py
@@ -37,9 +37,9 @@ class RankerParameters:
     wha_power: Final[float] = 1.0
 
     # Weighted to slightly positive HA.
-    dec_diff_less_40: ClassVar[npt.NDArray[float]] = field(default=np.array([3., 0., -0.08]))
+    dec_diff_less_40: npt.NDArray[float] = field(default_factory=lambda: np.array([3., 0., -0.08]))
     # Weighted to 0 HA if Xmin > 1.3.
-    dec_diff: ClassVar[npt.NDArray[float]] = field(default=np.array([3., 0.1, -0.06]))
+    dec_diff: npt.NDArray[float] = field(default_factory=lambda: np.array([3., 0.1, -0.06]))
 
     score_combiner: Callable[[npt.NDArray[float]], npt.NDArray[float]] = _default_score_combiner
 
