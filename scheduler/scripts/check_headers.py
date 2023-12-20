383e05ffbc3a519afb01aad5bc5b0d9d1659853e
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-12-19 15:16:46 -1000

    SCHED-533: Copyright update.

--- scheduler/scripts/check_headers.py
+++ scheduler/scripts/check_headers.py
@@ -1,38 +1,56 @@
-# Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
+# Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
 # For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
+import os
+import datetime
+import re
 from pathlib import Path
 
 from definitions import ROOT_DIR
 
-# Checks the headers of all .py files to make sure that they contain the above Copyright message.
+year = datetime.datetime.now().year
+year_range_pattern = r'Copyright \(c\) (\d{4})-(\d{4})'
+new_copyright = f'# Copyright (c) 2016-{year} Association of Universities for Research in Astronomy, Inc. (AURA)\n'
+second_line = '# For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause\n\n'
 
-HEADER = ('# Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)\n'
-          '# For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause')
+
+def update_file(file_path):
+    updated_lines = []
+    found_copyright = False
+
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    for line in lines:
+        match = re.search(year_range_pattern, line)
+        if match:
+            found_copyright = True
+            old_year = int(match.group(2))
+            if old_year < year:
+                print(f'Updating copyright year in file {file_path} from {old_year} to {year}')
+                updated_line = re.sub(year_range_pattern, f'Copyright (c) 2016-{year}', line)
+                updated_lines.append(updated_line)
+            else:
+                updated_lines.append(line)
+        else:
+            updated_lines.append(line)
+
+    if not found_copyright:
+        print(f'Adding copyright to file: {file_path}')
+        updated_lines.insert(0, new_copyright)
+        updated_lines.insert(1, second_line)
+
+    with open(file_path, 'w') as file:
+        file.writelines(updated_lines)
 
 
-def pre_adder(file):
-    with open(file, 'r+') as f:
-        content = f.read()
-        f.seek(0, 0)
-        f.write(HEADER + '\n' + content)
+def update_copyright(directory):
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith('.py'):
+                update_file(os.path.join(root, filename))
 
 
 if __name__ == '__main__':
-    COPYRIGHT = '# Copyright'
-
-    bad_files = []
-    for path in Path(ROOT_DIR).rglob('*.py'):
-        with open(path, 'r') as f:
-            if not f.readline().startswith(COPYRIGHT):
-                bad_files.append(path)
-
-    if bad_files:
-        print('*** FILES MISSING COPYRIGHT ***')
-        for bad_file in bad_files:
-            print(bad_file)
-            pre_adder(bad_file)
-        exit(1)
-    else:
-        print('All files copyrighted.')
-        exit(0)
+    update_copyright(Path(ROOT_DIR) / 'scheduler')
+    update_file(Path(ROOT_DIR) / 'definitions.py')
