6b31bc9d6ec518b973749428e157a7eb0b1bd7c8
Sebastian Raaphorst <sraaphorst@gmail.com>
2024-01-07 23:36:31 -1000

    SCHED-562: Improvements.

--- definitions.py
+++ definitions.py
@@ -4,10 +4,33 @@
 import os
 import sys
 import logging
+from enum import IntEnum
+from typing import NoReturn
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-if len(sys.argv) > 1:
-    DEFAULT_LOGGING_LEVEL = None
+
+class LoggingLevels(IntEnum):
+    INFO = logging.INFO
+    WARNING = logging.WARNING
+    ERROR = logging.ERROR
+    CRITICAL = logging.CRITICAL
+    FATAL = logging.FATAL
+    OFF = 100
+
+
+def _print_usage() -> NoReturn:
+    print(f'Usage: {sys.argv[0]} <logging_level>')
+    sys.exit(1)
+
+
+# Default is full logging.
+if len(sys.argv) == 1:
+    DEFAULT_LOGGING_LEVEL = LoggingLevels.INFO
+elif len(sys.argv) == 2:
+    try:
+        DEFAULT_LOGGING_LEVEL = LoggingLevels[sys.argv[1].upper()]
+    except KeyError:
+        _print_usage()
 else:
-    DEFAULT_LOGGING_LEVEL = logging.INFO
+    _print_usage()
