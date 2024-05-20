8d698c81d06f785880e8bd0f69c13199c1a00924
Sergio <sergio.troncoso@noirlab.edu>
2024-05-20 17:12:46 -0400

    SCHED-690: Add websocket comm and new params handler

--- scheduler/main.py
+++ scheduler/main.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2016-2024 Association of Universities for Research in Astronomy, Inc. (AURA)
 # For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 import asyncio
+import json
 import os
 
 import uvicorn
@@ -19,6 +20,7 @@ from scheduler.connection_manager import ConnectionManager
 from scheduler.core.builder.modes import SchedulerModes
 from scheduler.core.components.ranker import RankerParameters
 from scheduler.core.service import Service
+from scheduler.params import SchedulerParameters
 
 heroku_port = os.environ.get("PORT")
 manager = ConnectionManager()
@@ -31,28 +33,17 @@ def root() -> JSONResponse:
                             "message": "Welcome to Server"})
 
 
-async def ping_server(websocket):
-    while True:
-        await websocket.ping()
-        await asyncio.sleep(5)
-
-
 @app.websocket("/ws/{client_id}")
 async def schedule_websocket(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
     try:
         while True:
-            data = await websocket.receive_text()
+            data = await websocket.receive_json()
             if data:
-                for n in Service().generate(SchedulerModes.VALIDATION,
-                                            Time("2018-10-01 08:00:00", format='iso', scale='utc'),
-                                            Time("2018-10-06 08:00:00", format='iso', scale='utc'),
-                                            ALL_SITES,
-                                            RankerParameters(),
-                                            True,
-                                            4,
-                                            None):
+                for n in Service().generate(**SchedulerParameters.from_json(data).__dict__):
                     await manager.send(n, websocket)
+            else:
+                raise ValueError('Missing parameters to create schedule')
     except WebSocketDisconnect:
         manager.disconnect(websocket)
 
@@ -69,7 +60,28 @@ async def websocket_client(request: Request):
         <h1>WebSocket Plans</h1>
         <h2>Your ID: <span id="ws-id"></span></h2>
         <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
+            <label for="startTime">Start:</label>
+            <input type="text" id="startTime" value="2018-10-01 08:00:00" autocomplete="off"/><br/>
+            <label for="endTime">End:</label>
+            <input type="text" id="endTime" value="2018-10-03 08:00:00" autocomplete="off"/><br/>
+            <label for="sites">Sites:</label>
+            <input type="text" id="sites" value="GN" autocomplete="off"/><br/>
+            <label for="mode">Mode:</label>
+            <input type="text" id="mode" value="VALIDATION" autocomplete="off"/><br/>
+            <label for="semesterVisibility">semester Visibility:</label>
+            <input type="text" id="semesterVisibility" value="True" autocomplete="off"/><br/>
+            <label for="numNightsToSchedule"> num nights to schedule:</label>
+            <input type="text" id="numNightsToSchedule" value="3" autocomplete="off"/><br/>
+            <label for="thesisFactor">thesis_actor:</label>
+            <input type="number" id="thesisFactor" value="1.1" autocomplete="off"/><br/>
+            <label for="power">power:</label>
+            <input type="number" id="power" value="2.0" autocomplete="off"/><br/>
+            <label for="metPower">met_power:</label>
+            <input type="number" id="metPower" value="1.0" autocomplete="off"/><br/>
+            <label for="visPower">vis_power:</label>
+            <input type="number" id="visPower" value= "1.0" autocomplete="off"/><br/>
+            <label for="whaPower">wha_power:</label>
+            <input type="number" id="whaPower" value= "1.0" autocomplete="off"/><br/>
             <button>Run</button>
         </form>
         <ul id='messages'>
@@ -78,6 +90,7 @@ async def websocket_client(request: Request):
             var client_id = Date.now()
             document.querySelector("#ws-id").textContent = client_id;
             var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+            
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
@@ -86,9 +99,35 @@ async def websocket_client(request: Request):
                 messages.appendChild(message)
             };
             function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
+                
+                var input1 = document.getElementById("startTime")
+                var input2 = document.getElementById("endTime")
+                var input3 = document.getElementById("sites")
+                var input4 = document.getElementById("mode")
+                var input5 = document.getElementById("semesterVisibility")
+                var input6 = document.getElementById("numNightsToSchedule")
+                var input7 = document.getElementById("thesisFactor")
+                var input8 = document.getElementById("power")
+                var input9 = document.getElementById("metPower")
+                var input10 = document.getElementById("visPower")
+                var input11 = document.getElementById("whaPower")
+                
+                var data = {
+                    startTime: input1.value,
+                    endTime: input2.value,
+                    sites: [input3.value ],
+                    schedulerMode: input4.value,
+                    semesterVisibility: input5.value,
+                    numNightsToSchedule: input6.value,
+                    rankerParameters: {
+                        thesisFactor: input7.value,
+                        power: input8.value,
+                        metPower: input9.value,
+                        visPower: input10.value,
+                        whaPower: input11.value,
+                    }
+                }
+                ws.send(JSON.stringify(data));
                 event.preventDefault()
             }
         </script>
---
+++ scheduler/params.py
@@ -0,0 +1,37 @@
+import json
+from dataclasses import dataclass, field
+from typing import final, Optional, FrozenSet
+
+from astropy.time import Time
+from lucupy.minimodel import Site, ALL_SITES
+
+from scheduler.core.builder.modes import SchedulerModes
+from scheduler.core.components.ranker import RankerParameters
+
+
+@final
+@dataclass
+class SchedulerParameters:
+    start: Time
+    end: Time
+    sites: FrozenSet[Site]
+    mode: SchedulerModes
+    ranker_parameters: RankerParameters = field(default_factory=RankerParameters)
+    semester_visibility: bool = True
+    num_nights_to_schedule: Optional[int] = None
+    program_file: Optional[str] = None
+
+    @staticmethod
+    def from_json(received_params: dict) -> 'SchedulerParameters':
+        return SchedulerParameters(Time(received_params['startTime'], format='iso', scale='utc'),
+                                   Time(received_params['endTime'], format='iso', scale='utc'),
+                                   frozenset([Site[received_params['sites'][0]]]) if len(received_params) < 2 else ALL_SITES,
+                                   SchedulerModes[received_params['schedulerMode']],
+                                   RankerParameters(thesis_factor=received_params['rankerParameters']['thesisFactor'],
+                                                    power=received_params['rankerParameters']['power'],
+                                                    wha_power=received_params['rankerParameters']['whaPower'],
+                                                    met_power=received_params['rankerParameters']['metPower'],
+                                                    vis_power=received_params['rankerParameters']['visPower']),
+                                   received_params['semesterVisibility'],
+                                   received_params['numNightsToSchedule'],
+                                   None)
