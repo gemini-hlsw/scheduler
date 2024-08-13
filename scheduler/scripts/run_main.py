a86934653f92210c720110c78fed5dfc5f7238d6
Sergio <sergio.troncoso@noirlab.edu>
2024-08-13 17:22:11 -0400

    SCHED-692: Modify run script

--- scheduler/scripts/run_main.py
+++ scheduler/scripts/run_main.py
@@ -141,6 +141,9 @@ def main(*,
             # morn_twi_slot = time2slots(time_slot_length, morn_twi_time - eve_twi_time)
             morn_twi_slot = night_events.num_timeslots_per_night[night_idx]
 
+            # Get initial conditions for the nights
+            initial_variants[site][night_idx] = collector.sources.origin.env.get_initial_conditions(site, night_date)
+
             # Get the weather events for the site for the given night date.
             # Get the VariantSnapshots for the times of the night where the variant changes.
             variant_changes_dict = collector.sources.origin.env.get_variant_changes_for_night(site, night_date)
@@ -151,7 +154,6 @@ def main(*,
                 # The closer to the first time slot, the more accurate, and the ordering on them will overwrite
                 # the previous values.
                 if variant_timeslot <= 0:
-                    initial_variants[site][night_idx] = variant_snapshot
                     continue
 
                 if variant_timeslot >= morn_twi_slot:
