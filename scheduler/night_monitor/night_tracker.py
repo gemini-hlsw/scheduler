ee2fb0aafa776a6d20dd710811343154a421a131
Diego Gomez <diego.gomez@noirlab.edu>
2025-11-24 11:56:25 -0300

    Use async function to track, add filter funcion to trigger new plans

--- scheduler/night_monitor/night_tracker.py
+++ scheduler/night_monitor/night_tracker.py
@@ -1,13 +1,18 @@
 # Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
 # For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
+import asyncio
+import datetime
+
 from lucupy import sky
 from astropy.time import Time
 from astropy import units as u
 from time import sleep
 from scheduler.clients.scheduler_queue_client import schedule_queue
 from scheduler.core.builder.modes import SchedulerModes, app_mode
-import datetime
+
+from scheduler.services.logger_factory import create_logger
+_logger = create_logger(__name__)
 
 __all__ = ["NightTracker"]
 
@@ -51,29 +56,56 @@ class NightTracker:
     # Add end of night event
     self.sorted_night_events.append((self.sorted_night_events[-1][0] + 5 * u.min, "End of Night"))
 
-  def start_tracking(self):
+    # Debugging output
+    _logger.debug(self)
+  
+  def should_trigger_plan(self, event):
+    """
+    Determines if a given event should trigger a new plan.
+    Args:
+        event (tuple): A tuple containing the event time and description.
+    Returns:
+        bool: True if the event should trigger a new plan, False otherwise.
+    """
+    trigger_events = [
+      "Evening 12° Twilight",
+      "Morning 12° Twilight",
+      "End of Night"
+    ]
+    for trigger in trigger_events:
+      if trigger in event[1]:
+        return True
+
+    # TODO: Add more conditions as needed
+    return False
+
+  async def start_tracking(self):
     """
     This should be a thread or async process that
     In RT should add events to the scheduler queue when an event time is reached
     In non-RT should add all events to the scheduler queue at once
     """
     if app_mode != SchedulerModes.OPERATION:
+      _logger.info("Starting non-real-time tracking of night events")
       for event_time, event_desc in self.sorted_night_events:
-        # TODO: Add proper event object to the queue
-        schedule_queue.add_schedule_event()
+        if self.should_trigger_plan((next_event_time, event_description)):
+          # TODO: Add proper event object to the queue
+          schedule_queue.add_schedule_event()
       return
 
     # Real-time mode
+    _logger.info("Starting real-time tracking of night events")
     while len(self.sorted_night_events) > 0:
       current_time = Time.now()
       next_event_time, event_description = self.sorted_night_events[0]
       if current_time >= next_event_time:
         print(f"Event Triggered: {event_description} at {current_time.iso}")
         self.sorted_night_events.pop(0)
-        # TODO: Add proper event object to the queue
-        schedule_queue.add_schedule_event()
+        if self.should_trigger_plan((next_event_time, event_description)):
+          # TODO: Add proper event object to the queue
+          schedule_queue.add_schedule_event()
       else:
-        sleep(self.CHECK_INTERVAL)  # Sleep for a while before checking again
+        await asyncio.sleep(self.CHECK_INTERVAL)  # Sleep for a while before checking again
 
   def __str__(self):
     """
