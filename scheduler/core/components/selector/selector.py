Fix mixed units in wind score calculation (#694)

scheduler/core/components/selector/selector.py
@@ -711,7 +711,7 @@ class Selector(SchedulerComponent):
         Calculate the effect of the wind conditions on the score of an observation.
         """
         wind = np.ones(len(azimuth))
-        az_wd = np.abs(azimuth.to_value() - variant.wind_dir.to_value())
+        az_wd = np.abs(azimuth.degree - variant.wind_dir.to_value())
         idx = np.where(np.logical_and(variant.wind_spd > Selector._wind_spd_bound,  # * u.m / u.s
                                       np.logical_or(az_wd <= Selector._wind_sep.to_value(),
                                                     360 - az_wd <= Selector._wind_sep.to_value())))[0]