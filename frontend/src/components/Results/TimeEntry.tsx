af49d3132f8cdfe431b1ecb7e04759a6826dfde4
Diego Gómez <diego.gomez@noirlab.edu>
2026-08-10 08:50:12 -0400

    Add event and icons to generated plan table

--- frontend/src/components/Results/TimeEntry.tsx
+++ frontend/src/components/Results/TimeEntry.tsx
@@ -17,6 +17,115 @@ import {
 } from "@/components/ui/table";
 import { cn } from "@/lib/utils";
 import { ObsClassBadge } from "./ObsClassBadge";
+import { FaCheck } from "react-icons/fa";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { IoHourglassOutline } from "react-icons/io5";
+
+function VisitRow({
+  visit,
+  site,
+  state,
+  beingExecuted = false,
+  drawTopBorder = false,
+  drawBotBorder = false,
+}: {
+  visit: Visit;
+  site: string;
+  state: string;
+  beingExecuted?: boolean;
+  drawTopBorder?: boolean;
+  drawBotBorder?: boolean;
+}) {
+  const tz = site === "GN" ? "Pacific/Honolulu" : "America/Santiago";
+
+  function fractionToPercentage(fraction: string): number {
+    const parts = fraction.split("/");
+    if (parts.length !== 2) {
+      throw new Error("Invalid fraction format");
+    }
+    const numerator = parseFloat(parts[0]);
+    const denominator = parseFloat(parts[1]);
+    if (denominator === 0) {
+      throw new Error("Denominator cannot be zero");
+    }
+    const percentage = (numerator / denominator) * 100;
+
+    return percentage;
+  }
+
+  const formatScore = (score: number) => {
+    return score.toFixed(2);
+  };
+
+  const scoreBodyTemplate = (visit: Visit) => {
+    return formatScore(visit.score);
+  };
+  const peakScoreBodyTemplate = (visit: Visit) => {
+    return formatScore(visit.peakScore);
+  };
+
+  const obsCompletionBodyTemplate = (visit: Visit) => {
+    return `${visit.completion} (${fractionToPercentage(
+      visit.completion,
+    ).toFixed(0)}%)`;
+  };
+
+  let icon = null;
+  switch (state) {
+    case "executed":
+      icon = <FaCheck className="text-green-500" />;
+      break;
+    case "executing":
+      icon = (
+        <AiOutlineLoading3Quarters className="animate-spin text-yellow-500" />
+      );
+      break;
+    case "scheduled":
+      icon = <IoHourglassOutline className="text-yellow-500" />;
+      break;
+    default:
+      icon = null;
+  }
+
+  return (
+    <TableRow
+      key={visit.startTime}
+      className={cn(
+        beingExecuted
+          ? "dark:bg-blue-700/50 bg-blue-400/50"
+          : "odd:bg-black/10 dark:odd:bg-white/10",
+        beingExecuted
+          ? "hover:dark:bg-blue-400/50 hover:bg-blue-700/50"
+          : "dark:hover:bg-white/30 hover:bg-black/30",
+        "*:p-0 *:px-2",
+        drawTopBorder ? "*:border-t-2 *:border-t-blue-500" : "",
+        drawBotBorder ? "*:border-b-2 *:border-b-blue-500" : "",
+      )}
+    >
+      <TableCell>{icon}</TableCell>
+      <TableCell>{visit.obsId}</TableCell>
+      <TableCell>
+        <ObsClassBadge obsClass={visit.obsClass} />
+      </TableCell>
+      <TableCell>
+        {new Date(visit.startTime).toLocaleString("en-UK", {
+          timeZone: tz,
+        })}
+      </TableCell>
+      <TableCell>{visit.atomStartIdx}</TableCell>
+      <TableCell>{visit.atomEndIdx}</TableCell>
+      <TableCell>{visit.instrument}</TableCell>
+      <TableCell>{visit.fpu}</TableCell>
+      <TableCell>{visit.disperser}</TableCell>
+      <TableCell>{visit.filters}</TableCell>
+      <TableCell>{visit.requiredConditions.cc}</TableCell>
+      <TableCell>{visit.requiredConditions.iq}</TableCell>
+      <TableCell>{obsCompletionBodyTemplate(visit)}</TableCell>
+      <TableCell>{peakScoreBodyTemplate(visit)}</TableCell>
+      <TableCell>{scoreBodyTemplate(visit)}</TableCell>
+    </TableRow>
+  );
+}
 
 export default function TimeEntry({
   timeEntry,
@@ -39,42 +148,84 @@ export default function TimeEntry({
       atomTimes: visit.atomTimes,
     }));
   }
-  const tz = site === "GN" ? "Pacific/Honolulu" : "America/Santiago";
-  const formatScore = (score: number) => {
-    return score.toFixed(2);
-  };
-
-  function fractionToPercentage(fraction: string): number {
-    const parts = fraction.split("/");
-    if (parts.length !== 2) {
-      throw new Error("Invalid fraction format");
-    }
-    const numerator = parseFloat(parts[0]);
-    const denominator = parseFloat(parts[1]);
-    if (denominator === 0) {
-      throw new Error("Denominator cannot be zero");
-    }
-    const percentage = (numerator / denominator) * 100;
-
-    return percentage;
-  }
-
-  const scoreBodyTemplate = (visit: Visit) => {
-    return formatScore(visit.score);
-  };
-  const peakScoreBodyTemplate = (visit: Visit) => {
-    return formatScore(visit.peakScore);
-  };
-
-  const obsCompletionBodyTemplate = (visit: Visit) => {
-    return `${visit.completion} (${fractionToPercentage(
-      visit.completion,
-    ).toFixed(0)}%)`;
-  };
 
   if (!timeEntry || !timeEntry.plan) return <div>No plan found</div>;
 
-  console.log(timeEntry);
+  const eventTime = new Date(timeEntry.event.time);
+  const executedVisits = [];
+  const beingExecutedVisits = [];
+  const scheduledVisits = [];
+  const visitRows = [];
+
+  for (const visit of timeEntry.plan.visits) {
+    if (new Date(visit.endTime) < eventTime) {
+      executedVisits.push(visit);
+    } else if (new Date(visit.startTime) > eventTime) {
+      scheduledVisits.push(visit);
+    } else {
+      beingExecutedVisits.push(visit);
+    }
+  }
+
+  for (let i = 0; i < executedVisits.length; i++) {
+    if (i === executedVisits.length - 1) {
+      visitRows.push(
+        <VisitRow
+          key={`executed-visit-${i}`}
+          visit={executedVisits[i]}
+          site={site}
+          state="executed"
+          drawBotBorder={beingExecutedVisits.length === 0}
+        />,
+      );
+    } else {
+      visitRows.push(
+        <VisitRow
+          key={`executed-visit-${i}`}
+          visit={executedVisits[i]}
+          site={site}
+          state="executed"
+        />,
+      );
+    }
+  }
+
+  for (let i = 0; i < beingExecutedVisits.length; i++) {
+    visitRows.push(
+      <VisitRow
+        key={`current-visit-${i}`}
+        visit={beingExecutedVisits[i]}
+        site={site}
+        state="executing"
+        beingExecuted={true}
+      />,
+    );
+  }
+
+  for (let i = 0; i < scheduledVisits.length; i++) {
+    if (i === 0) {
+      visitRows.push(
+        <VisitRow
+          key={`scheduled-visit-${i}`}
+          visit={scheduledVisits[i]}
+          site={site}
+          state="scheduled"
+          drawTopBorder={
+            beingExecutedVisits.length === 0 && executedVisits.length === 0
+          }
+        />,
+      );
+    } else {
+      visitRows.push(
+        <VisitRow
+          key={`scheduled-visit-${i}`}
+          visit={scheduledVisits[i]}
+          site={site}
+          state="scheduled"
+        />,
+      );
+    }
+  }
 
   return (
     <Accordion
@@ -107,6 +258,7 @@ export default function TimeEntry({
                   "*:h-6 *:font-bold",
                 )}
               >
+                <TableHead></TableHead>
                 <TableHead>Observation Id</TableHead>
                 <TableHead>Observation Class</TableHead>
                 <TableHead>Start Time</TableHead>
@@ -123,38 +275,7 @@ export default function TimeEntry({
                 <TableHead>Score</TableHead>
               </TableRow>
             </TableHeader>
-            <TableBody>
-              {timeEntry.plan.visits.map((visit: Visit) => (
-                <TableRow
-                  key={visit.startTime}
-                  className={cn(
-                    "odd:bg-black/10 dark:odd:bg-white/10 *:p-0 *:px-2",
-                    "dark:hover:bg-white/30 hover:bg-black/30",
-                  )}
-                >
-                  <TableCell>{visit.obsId}</TableCell>
-                  <TableCell>
-                    <ObsClassBadge obsClass={visit.obsClass} />
-                  </TableCell>
-                  <TableCell>
-                    {new Date(visit.startTime).toLocaleString("en-UK", {
-                      timeZone: tz,
-                    })}
-                  </TableCell>
-                  <TableCell>{visit.atomStartIdx}</TableCell>
-                  <TableCell>{visit.atomEndIdx}</TableCell>
-                  <TableCell>{visit.instrument}</TableCell>
-                  <TableCell>{visit.fpu}</TableCell>
-                  <TableCell>{visit.disperser}</TableCell>
-                  <TableCell>{visit.filters}</TableCell>
-                  <TableCell>{visit.requiredConditions.cc}</TableCell>
-                  <TableCell>{visit.requiredConditions.iq}</TableCell>
-                  <TableCell>{obsCompletionBodyTemplate(visit)}</TableCell>
-                  <TableCell>{peakScoreBodyTemplate(visit)}</TableCell>
-                  <TableCell>{scoreBodyTemplate(visit)}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
+            <TableBody>{visitRows}</TableBody>
           </Table>
           <Table>
             <TableHeader>
